14f6fe229f7dace17bfbb844833e0d5561c6933d
Sunny (Hui-Shan) Cheng <huishan.cheng@studio.unibo.it>
2020-11-05 02:43:07 +0000

    adding testing cases

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,21 @@
 const request = require('request');
-const arg = process.argv.slice(2);
-const breedName = arg[0];
-let url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
 
+const fetchBreedDescription = (breedName, callback) => {
+  let url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
   request(url, (error, response, body) => {
-    if (error) {
-      console.log(`Request failed: ${error}`);
-    } else {
-      const data = JSON.parse(body); 
-      if (data.length === 0) {//when there is no query(breedName) it will show [] on page
-        console.log("breed not found");
-      } else {
-        console.log(data[0].description);
-      }
+    if (error) {//handle request fail like a typo in the url
+      return callback(error, null);
     } 
-});
+    const data = JSON.parse(body); 
+    if (data.length === 0) {//when there is no query(breedName) it will show [] on page
+    return callback("breed not found",null);
+  } 
+    return callback(null, data[0].description);
+  });
+};
 
+
+
+
+module.exports = { fetchBreedDescription };
---
+++ index.js
@@ -0,0 +1,10 @@
+const { fetchBreedDescription } = require('./breedFetcher');
+const breedName = process.argv[2];
+
+fetchBreedDescription(breedName, (error, description) => {//we use request wich fetch result async, the new fetch function also needs to be async, it should therefore accept a callback cuz async can't simply return data.
+if (error) {
+  console.log('Error fetch details: ',error);
+} else {
+  console.log(description);
+}
+});
