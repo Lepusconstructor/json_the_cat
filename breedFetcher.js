07d6764f01f4cd2114ff8b03d86a6673b011814f
Sunny (Hui-Shan) Cheng <huishan.cheng@studio.unibo.it>
2020-11-05 02:46:18 +0000

    adding comments

--- breedFetcher.js
+++ breedFetcher.js
@@ -7,9 +7,10 @@ const fetchBreedDescription = (breedName, callback) => {
     if (error) {//handle request fail like a typo in the url
       return callback(error, null);
     } 
+    //if no request fail, but other error like an invalid name
     const data = JSON.parse(body); 
-    if (data.length === 0) {//when there is no query(breedName) it will show [] on page
-    return callback("breed not found",null);
+    if (data.length === 0) {//when there is no valid query(breedName) it will show [] on page
+    return callback("breed not found", null);
   } 
     return callback(null, data[0].description);
   });
